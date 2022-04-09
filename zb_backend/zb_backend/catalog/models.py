9456ee48e583c0489fb0870e87d9a1cb0d8b52b4
Ariel Curtiido <aicurtido@gmail.com>
2022-04-08 21:16:08 -0400

    New models added

--- zb_backend/zb_backend/catalog/models.py
+++ zb_backend/zb_backend/catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(null=False, max_length=50)
+
+class Product(models.Model):
+    sku = models.CharField(null=False, max_length=10, unique=True)
+    name = models.CharField(null=False, max_length=50)
+    price = models.FloatField(null=False, default=0.0)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+
+class ProductTrack(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    visits = models.IntegerField(null=False, default=0)
+
+    
+
--- zb_backend/zb_backend/settings.py
+++ zb_backend/zb_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zb_backend.catalog',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
