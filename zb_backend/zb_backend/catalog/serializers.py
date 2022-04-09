5bf59079f62273fe11ac084eecc8d31d87447331
Ariel Curtiido <aicurtido@gmail.com>
2022-04-09 11:54:28 -0400

    Authentication and APIs endpoints

---
+++ zb_backend/zb_backend/catalog/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User, Group
+from zb_backend.catalog.models import Product, ProductTrack, Brand
+from django.db import transaction
+from rest_framework import serializers
+
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+
+class BrandSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['name']
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['sku', 'name', 'price', 'brand']
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            instance = super().create(validated_data)
+            ProductTrack.objects.create(product=instance)
+
+        return instance
--- zb_backend/zb_backend/catalog/views.py
+++ zb_backend/zb_backend/catalog/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import Group, User
+from django.db import transaction
+from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from zb_backend.catalog.models import Brand, Product, ProductTrack
+from zb_backend.catalog.serializers import (BrandSerializer, GroupSerializer,
+                                             ProductSerializer, UserSerializer)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['retrieve', 'list']:
+            return [permissions.AllowAny()]
+
+        return super().get_permissions()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        product_track_queryset = ProductTrack.objects.select_for_update().filter(
+            product=instance)
+
+        with transaction.atomic():
+            product_track = product_track_queryset.first()
+            product_track.visits += 1
+            product_track.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
