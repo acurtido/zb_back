5ca1c951312afe36ff171a560b8bd242be70c5b2
Ariel Curtido <aicurtido@gmail.com>
2023-01-27 00:15:21 -0300

    Include openapi schema

--- zb_backend/zb_backend/urls.py
+++ zb_backend/zb_backend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
+
 from zb_backend.catalog import views
 
 router = routers.DefaultRouter()
@@ -12,5 +14,10 @@ router.register(r'products-track', views.ProductTrackViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('openapi', get_schema_view(
+        title="ZB_Backend API",
+        description="API for all things",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
